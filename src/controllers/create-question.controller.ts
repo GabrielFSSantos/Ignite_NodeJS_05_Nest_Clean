f91296195a32bc6a628537771044d89a6df53daa
Gabriel Santos <59262903+GabrielFSSantos@users.noreply.github.com>
2024-06-05 17:59:52 -0300

    Creating authentication decorator

--- src/controllers/create-question.controller.ts
+++ src/controllers/create-question.controller.ts
@@ -10,6 +10,8 @@ import { PrismaService } from 'src/prisma/prisma.service'
 import { z } from 'zod'
 import { ZodValidationPipe } from 'src/pipes/zod-validation-pipe'
 import { JwtAuthGuard } from 'src/auth/jwt.auth.guard'
+import { CurrentUserDecorator } from 'src/auth/current-user.decorator'
+import { UserPayload } from 'src/auth/jwt.strategy'
 
 const createQuestionBodySchema = z.object({
   title: z.string(),
@@ -28,11 +30,13 @@ export class CreateQuestionController {
   @Post()
   @HttpCode(201)
   @UsePipes(new ZodValidationPipe(createQuestionBodySchema))
-  async handle(@Body() body: CreateQuestionBodySchema) {
-    const { title, slug, content, authorId } = body
-
+  async handle(
+    @Body() body: CreateQuestionBodySchema,
+    @CurrentUserDecorator() user: UserPayload,
+  ) {
     return {
-      teste: 'teste',
+      body,
+      user,
     }
   }
 }
