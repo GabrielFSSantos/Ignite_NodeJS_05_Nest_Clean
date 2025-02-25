74bd0dcef8fa0294e7d0aeb7a0c8c49e7f363db9
GabrielFSSantos <gabrielfelipesouzasantos@gmail.com>
2025-02-25 13:08:58 -0300

    Controller: Edit Question

---
+++ src/infra/http/controllers/edit-question.controller.ts
@@ -0,0 +1,39 @@
+import { Body, Controller, HttpCode, Param, Put } from '@nestjs/common'
+import { z } from 'zod'
+import { ZodValidationPipe } from '@/infra/http/pipes/zod-validation-pipe'
+import { CurrentUserDecorator } from '@/infra/auth/current-user.decorator'
+import { UserPayload } from '@/infra/auth/jwt.strategy'
+import { EditQuestionUseCase } from '@/domain/forum/application/use-cases/edit-question'
+
+const editQuestionBodySchema = z.object({
+  title: z.string(),
+  content: z.string(),
+})
+
+type EditQuestionBodySchema = z.infer<typeof editQuestionBodySchema>
+
+const bodyValidationPipe = new ZodValidationPipe(editQuestionBodySchema)
+
+@Controller('/questions/:id')
+export class EditQuestionController {
+  constructor(private editQuestion: EditQuestionUseCase) {}
+
+  @Put()
+  @HttpCode(204)
+  async handle(
+    @Param('id') questionId: string,
+    @Body(bodyValidationPipe) body: EditQuestionBodySchema,
+    @CurrentUserDecorator() user: UserPayload,
+  ) {
+    const { title, content } = body
+    const { sub: authorId } = user
+
+    await this.editQuestion.execute({
+      questionId,
+      authorId,
+      title,
+      content,
+      attachmentsIds: [],
+    })
+  }
+}
