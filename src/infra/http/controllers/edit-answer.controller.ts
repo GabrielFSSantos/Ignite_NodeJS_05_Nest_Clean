3a1821f729f8775a3021f0c82c0d7f101a86bacd
GabrielFSSantos <gabrielfelipesouzasantos@gmail.com>
2025-02-26 11:28:08 -0300

    Controller: Edit Response

---
+++ src/infra/http/controllers/edit-answer.controller.ts
@@ -0,0 +1,48 @@
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  HttpCode,
+  Param,
+  Put,
+} from '@nestjs/common'
+import { z } from 'zod'
+import { ZodValidationPipe } from '@/infra/http/pipes/zod-validation-pipe'
+import { CurrentUserDecorator } from '@/infra/auth/current-user.decorator'
+import { UserPayload } from '@/infra/auth/jwt.strategy'
+import { EditAnswerUseCase } from '@/domain/forum/application/use-cases/edit-answer'
+
+const editAnswerBodySchema = z.object({
+  content: z.string(),
+})
+
+type EditAnswerBodySchema = z.infer<typeof editAnswerBodySchema>
+
+const bodyValidationPipe = new ZodValidationPipe(editAnswerBodySchema)
+
+@Controller('/answers/:id')
+export class EditAnswerController {
+  constructor(private editAnswer: EditAnswerUseCase) {}
+
+  @Put()
+  @HttpCode(204)
+  async handle(
+    @Param('id') answerId: string,
+    @Body(bodyValidationPipe) body: EditAnswerBodySchema,
+    @CurrentUserDecorator() user: UserPayload,
+  ) {
+    const { content } = body
+    const { sub: authorId } = user
+
+    const result = await this.editAnswer.execute({
+      answerId,
+      authorId,
+      content,
+      attachmentsIds: [],
+    })
+
+    if (result.isLeft()) {
+      throw new BadRequestException()
+    }
+  }
+}
