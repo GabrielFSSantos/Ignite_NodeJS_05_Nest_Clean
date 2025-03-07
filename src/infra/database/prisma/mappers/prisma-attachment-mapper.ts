e4fe862708a71806e764e6b3992131cdaad0f984
GabrielFSSantos <gabrielfelipesouzasantos@gmail.com>
2025-03-07 12:17:54 -0300

    Testing upload controller

---
+++ src/infra/database/prisma/mappers/prisma-attachment-mapper.ts
@@ -0,0 +1,23 @@
+import { UniqueEntityId } from '@/core/entities/unique-entity-id'
+import { Attachment } from '@/domain/forum/enterprise/entities/attachment'
+import { Attachment as PrismaAttachment, Prisma } from '@prisma/client'
+
+export class PrismaAttachmentMapper {
+  static toDomain(raw: PrismaAttachment): Attachment {
+    return Attachment.create(
+      {
+        title: raw.title,
+        url: raw.url,
+      },
+      new UniqueEntityId(raw.id),
+    )
+  }
+
+  static toPrisma(raw: Attachment): Prisma.AttachmentUncheckedCreateInput {
+    return {
+      id: raw.id.toString(),
+      title: raw.title,
+      url: raw.url,
+    }
+  }
+}
--- src/infra/http/controllers/upload-attachment.controller.ts
+++ src/infra/http/controllers/upload-attachment.controller.ts
@@ -7,14 +7,18 @@ import {
   ParseFilePipe,
   MaxFileSizeValidator,
   FileTypeValidator,
+  BadRequestException,
 } from '@nestjs/common'
 import { FileInterceptor } from '@nestjs/platform-express'
 
-// import { UploadAttachmentUseCase } from '@/domain/forum/application/use-cases/get-question-by-slug'
+import { UploadAndCreateAttachmentUseCase } from '@/domain/forum/application/use-cases/upload-and-create-attachment'
+import { InvalidAttachmentTypeError } from '@/domain/forum/application/use-cases/errors/invalid-attachment-type'
 
 @Controller('/attachments')
 export class UploadAttachmentController {
-  // constructor(private uploadAttachmentUseCase: UploadAttachmentUseCase) {}
+  constructor(
+    private uploadAndCreateAttachmentUseCase: UploadAndCreateAttachmentUseCase,
+  ) {}
 
   @Post()
   @HttpCode(201)
@@ -32,6 +36,27 @@ export class UploadAttachmentController {
     )
     file: Express.Multer.File,
   ) {
-    console.log(file)
+    const result = await this.uploadAndCreateAttachmentUseCase.execute({
+      fileName: file.originalname,
+      fileType: file.mimetype,
+      body: file.buffer,
+    })
+
+    if (result.isLeft()) {
+      const error = result.value
+
+      switch (error.constructor) {
+        case InvalidAttachmentTypeError:
+          throw new BadRequestException(error.message)
+        default:
+          throw new BadRequestException(error.message)
+      }
+    }
+
+    const { attachment } = result.value
+
+    return {
+      attachmentId: attachment.id.toString(),
+    }
   }
 }
