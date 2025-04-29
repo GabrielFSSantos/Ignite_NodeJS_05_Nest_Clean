8b99ca3b1ad9431275141c9d84af5ee86956f4cf
GabrielFSSantos <gabrielfelipesouzasantos@gmail.com>
2025-04-29 19:16:18 -0300

    Creating Redis Service

--- src/infra/env/env.service.ts
+++ src/infra/env/env.service.ts
@@ -1,12 +1,22 @@
 import { Injectable } from '@nestjs/common'
 import { ConfigService } from '@nestjs/config'
-import { Env } from './env'
+import { Env, envSchema } from './env'
 
 @Injectable()
 export class EnvService {
-  constructor(private configService: ConfigService<Env, true>) {}
+  private readonly values: Env
 
-  get<T extends keyof Env>(key: T) {
-    return this.configService.get<T>(key, { infer: true })
+  constructor() {
+    this.values = envSchema.parse(process.env)
   }
+
+  get<K extends keyof Env>(key: K): Env[K] {
+    return this.values[key]
+  }
+
+  // constructor(private configService: ConfigService<Env, true>) {}
+
+  // get<T extends keyof Env>(key: T) {
+  //   return this.configService.get<T>(key, { infer: true })
+  // }
 }
